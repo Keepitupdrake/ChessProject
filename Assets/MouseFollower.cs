0630c5b33f84a3837cddbc3ba074540e686e6358
Andrey Murilo <andreygugu9@gmail.com>
2020-02-23 22:56:12 -0300

    Implemented Drag n Drop feature

---
+++ Assets/MouseFollower.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    Vector3 desiredPosition = new Vector3();
+    public Vector3 offset = new Vector3(1f, 1f, 10f);
+    public Piece draggedPiece;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Moves the object
+        desiredPosition = Input.mousePosition + offset;
+        transform.position = Camera.main.ScreenToWorldPoint(desiredPosition);
+
+        if (draggedPiece != null)
+            draggedPiece.transform.position = Camera.main.ScreenToWorldPoint(desiredPosition);
+
+        if (Input.GetMouseButtonUp(0) && draggedPiece != null)
+            draggedPiece.dragged = true;
+
+        if (Input.GetMouseButtonDown(0) && draggedPiece != null && draggedPiece.dragged == true)
+        {
+            Drop();
+        }
+    }
+  
+    // Drop the Piece on the board
+    public void Drop()
+    {
+        draggedPiece.transform.position = Camera.main.ScreenToWorldPoint(desiredPosition);
+        draggedPiece.dragged = false;
+        draggedPiece = null;
+    }
+
+   
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Piece : MonoBehaviour
+{
+
+    public bool FirstMove { get; set; } = true;
+    public bool dragged = false;
+    public MouseFollower follow;
+
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool CanMove(Vector2 targetPos)
+    {
+
+        return true;
+    }
+    public abstract void Move();
+
+    private void OnMouseDown()
+    {
+        if (dragged == false)
+            follow.draggedPiece = this;
+    }
+}
+
+ 
